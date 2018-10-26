28c71a5cd3d5d1331b74678c1f9c9c8276ee7dc1
yuna <chldbsk8911@naver.com>
2018-10-26 15:08:02 +0900

    membervo 추가

---
+++ gittest/src/test/MemberVO.java
@@ -0,0 +1,22 @@
+package test;
+
+public class MemberVO {
+	private String no;
+	private String age;
+	
+	public String getNo() {
+		return no;
+	}
+	public void setNo(String no) {
+		this.no = no;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	
+
+}
